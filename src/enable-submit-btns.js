68f9370af8a38dfff1bb440ed5aad39f5f7fe4ad
Massimo Cassandro <massimo.cassandro@gmail.com>
2023-04-20 16:13:32 +0200

    upg

--- src/enable-submit-btns.js
+++ src/enable-submit-btns.js
@@ -1,5 +1,5 @@
-export function enable_submit_btns() {
-  document.querySelectorAll('[type=submit], [type=button]:not([data-disabled])').forEach(btn => {
+export function enable_submit_btns(context = document) {
+  context.querySelectorAll('[type=submit], [type=button]:not([data-disabled])').forEach(btn => {
     btn.disabled = false;
   });
 }
